528695afa0cb3f6d30a721dbef8af8ef52850024
pputthanon <p.putthanon@gmail.com>
2023-10-18 17:35:42 +0700

    add Book to cart from front to back

--- src/controllers/cart-controller.js
+++ src/controllers/cart-controller.js
@@ -4,15 +4,36 @@ exports.addBook = async (req, res, next) => {
   try {
     const { id } = req.user;
     const { productsId } = req.body;
-    const oldproduct = [].find(
-      (el) => el.productsId == productsId && el.userId == id
-    );
+
+    // const oldproduct = await prisma.cart.findMany({});
+    // const test = oldproduct.find(
+    //   (el) => el.productsId == productsId && el.userId == id
+    // );
+
+    // if (test) {
+    //   await prisma.cart.update({
+    //     where: {
+    //         id
+    //     },
+    //   })
+    // }
+
+    const oldproduct = await prisma.cart.findFirst({
+      where: {
+        productsId,
+        userId: id,
+      },
+    });
+    console.log(oldproduct);
     if (oldproduct) {
-      await prisma.cart.update({
+      await prisma.cart.updateMany({
         data: {
           userId: id,
           productsId: productsId,
           amount: oldproduct.amount + 1,
+        },
+        where: {
+          id: oldproduct.id,
         },
       });
     } else {
